f2d00e05a1c926aaf0291ecb7ce2c1a5688412f8
farhodyusupov <farxody98@gmail.com>
2023-03-09 15:37:52 +0900

    checkPlatfrom

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,8 @@
 import 'package:tavr/data/dummy_data.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:tavr/screens/more_info_screen.dart';
+import 'package:tavr/widget/const.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,27 +16,106 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.all(50),
+
+        padding: const EdgeInsets.all(50),
         alignment: Alignment.center,
         child: ListView.builder(
           itemCount: dummyList.length,
           itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MoreInfoScreen(
-                      patientId: dummyList[index].id,
-                    ),
+            return Container(
+              margin: const EdgeInsets.symmetric(horizontal: 40),
+              alignment: Alignment.center,
+              height: 70,
+              decoration: BoxDecoration(
+                  color: index % 2 == 0
+                      ? Constants.lineColor1
+                      : Constants.lineColor2),
+              child: index == 0
+                  ? Column(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          color: Colors.white,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 40,
+                                child: Text("Id", style: TextStyle(),),
+                              ),
+                              Container(
+                                width: 100,
+                                child: Text("Gender"),
+                              ),
+                              Container(
+                                width: 100,
+                                child: Text("Age"),
+                              ),
+                              const SizedBox(
+                                width: 100,
+                              )
+                            ],
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              width: 40,
+                              child: Text(dummyList[index].id.toString()),
+                            ),
+                            Container(
+                              width: 100,
+                              child: Text(dummyList[index].sex),
+                            ),
+                            Container(
+                              width: 100,
+                              child: Text(dummyList[index].age.toString()),
+                            ),
+                            detailedInfoButton(dummyList[index].id)
+                          ],
+                        ),
+                      ],
+                    )
+                  : Row(
+                children: [
+                  Container(
+                    width: 40,
+                    child: Text(dummyList[index].id.toString()),
                   ),
-                );
-              },
-              child: Text(
-                  "id: ${dummyList[index].id}   gender  ${dummyList[index].sex}   age   ${dummyList[index].age}, is web: $kIsWeb"),
+                  Container(
+                    width: 100,
+                    child: Text(dummyList[index].sex),
+                  ),
+                  Container(
+                    width: 100,
+                    child: Text(dummyList[index].age.toString()),
+                  ),
+                  detailedInfoButton(dummyList[index].id)
+                ],
+              ),
             );
           },
         ),
+      ),
+    );
+  }
+
+  Widget detailedInfoButton(int id){
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => MoreInfoScreen(
+                    patientId: id)));
+      },
+      child: Container(
+        height: 40,
+        width: 100,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.lightBlue.shade500),
+        child: Text("detailed info"),
       ),
     );
   }
---
+++ lib/widget/const.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Constants{
+   static  Color lineColor1 = Colors.lightBlue.withOpacity(0.1);
+   static  Color lineColor2 = Colors.white;
+}
+
+
+class TextStyles{
+
+
+}
